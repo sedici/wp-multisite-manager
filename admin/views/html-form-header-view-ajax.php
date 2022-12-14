b114276923d152b3b858cd780d7d4c51a306c1ac
lauj32 <ljosinsaller@gmail.com>
2022-12-14 10:27:35 -0300

    Modifique formulario para crear el header y el footer

--- admin/views/html-form-header-view-ajax.php
+++ admin/views/html-form-header-view-ajax.php
@@ -2,22 +2,25 @@
 <div id="notice_result"></div>
 <table class="form-table">
     <thead>
-        <tr >
+        <tr>
             <th scope='row'>Texto</th>
             <th scope='row'>Enlace del texto</th>
             <th scope='row'>Imagen</th>
             <th scope='row'>Enlace de la imagen</th>
         </tr>
     </thead>
-    <tbody id="list_repo">
-    </tbody>
-    <tfoot >
+
+    <hr>
+    <tbody id="form-repo">
         <tr>
-            <td colspan="4" style="text-align:right" ><button class="button button-primary agregar-nuevo-repo"> Agregar nuevo repositorio</button></td>
+            <th scope='row'> <input type="text"> </th>
+            <th scope='row'> <input type="text"> </th>
+            <th scope='row'> <input type="file"> </th>
+            <th scope='row'> <input type="text"> </th>
         </tr>
-  </tfoot>
-</table>
-<hr>
-<div id="form-repo" >
+    </tbody>
+
     
-</div>
+</table>
+
+
