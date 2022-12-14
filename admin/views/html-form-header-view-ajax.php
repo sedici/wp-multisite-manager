5e8483dd2d785bc556eac36ef8d367c60f1ff866
lauj32 <ljosinsaller@gmail.com>
2022-12-14 12:46:51 -0300

    agregue css al formulario del header

--- admin/views/html-form-header-view-ajax.php
+++ admin/views/html-form-header-view-ajax.php
@@ -4,30 +4,30 @@
 
 <div>
     
-    <div">
+    <div class="general-form-field">
         <h2 class="filds-titles"> Texto </h2>
         <input type="text">
     </div>
 
 
-    <div>
+    <div class="general-form-field">
         <h2 class="filds-titles"> Enlace del texto </h2>
         <input type="text">
     </div>
 
 
-    <div>
+    <div class="general-form-field">
         <h2 class="filds-titles"> Imagen </h2>
         <input type="file">
     </div>
 
-    <div>
+    <div class="general-form-field">
         <h2 class="filds-titles"> Enlace de la imagen </h2>
         <input type="text">
     </div>
 
 
-    <div>
+    <div class="general-form-field">
         <button id="save-changes-button" type="button">Guardar cambios</button>
     </div>
      
