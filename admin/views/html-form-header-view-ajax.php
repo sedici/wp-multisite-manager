627362279a57c41e6ef646c87da80c7088f67587
lauj32 <ljosinsaller@gmail.com>
2022-12-14 12:42:15 -0300

    cambie el formulario por divs en lugar de una tabla

--- admin/views/html-form-header-view-ajax.php
+++ admin/views/html-form-header-view-ajax.php
@@ -2,56 +2,37 @@
 
 <hr>
 
-<table class="Table-container">
+<div>
     
-    <tr>
-        <th class="table-headings" scope="row">Texto</th>
-    </tr>
-
-    <tr>
-        <td> 
-            <input type="text">
-        </td>
-    </tr>
-
-    <tr>
-        <th class="form-title" scope="row">Enlace del texto</th>
-    </tr>
-
-    <tr>
-        <td> 
-            <input type="text">
-        </td>
-    </tr>
-
-    <tr>
-        <th class="form-title" scope="row">Imagen</th>
-    </tr>
-
-    <tr>
-        <td> 
-            <input type="file">
-        </td>
-    </tr>
-
-    <tr>
-        <th class="form-title" scope="row">Enlace de la imagen</th>
-    </tr>
-
-    <tr>
-        <td> 
-            <input type="text">
-        </td>
-    </tr>
+    <div">
+        <h2 class="filds-titles"> Texto </h2>
+        <input type="text">
+    </div>
 
 
-    <tr>
-        <td> 
-            <button id="save-changes-button" type="button">Guardar cambios</button>
-        </td>
-    </tr>
+    <div>
+        <h2 class="filds-titles"> Enlace del texto </h2>
+        <input type="text">
+    </div>
 
-</table>
+
+    <div>
+        <h2 class="filds-titles"> Imagen </h2>
+        <input type="file">
+    </div>
+
+    <div>
+        <h2 class="filds-titles"> Enlace de la imagen </h2>
+        <input type="text">
+    </div>
+
+
+    <div>
+        <button id="save-changes-button" type="button">Guardar cambios</button>
+    </div>
+     
+
+</div>
 
 <hr>
 
