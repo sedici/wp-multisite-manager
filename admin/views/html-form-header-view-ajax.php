95ae50e633482b157950bd336b32e1277c68c96a
lauj32 <ljosinsaller@gmail.com>
2022-12-14 10:29:45 -0300

    agrego archivo css para los formularios

---
+++ admin/views/html-form-header-view-ajax.php
@@ -0,0 +1,26 @@
+<h1>Header</h1>
+<div id="notice_result"></div>
+<table class="form-table">
+    <thead>
+        <tr>
+            <th scope='row'>Texto</th>
+            <th scope='row'>Enlace del texto</th>
+            <th scope='row'>Imagen</th>
+            <th scope='row'>Enlace de la imagen</th>
+        </tr>
+    </thead>
+
+    <hr>
+    <tbody id="form-repo">
+        <tr>
+            <th scope='row'> <input type="text"> </th>
+            <th scope='row'> <input type="text"> </th>
+            <th scope='row'> <input type="file"> </th>
+            <th scope='row'> <input type="text"> </th>
+        </tr>
+    </tbody>
+
+    
+</table>
+
+
