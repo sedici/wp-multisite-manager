b6695bd9a88bbe1dbb84e6da5af62caff20f18ce
lauj32 <ljosinsaller@gmail.com>
2022-12-15 09:36:11 -0300

    agregue comentario a pagina de configuracion del header y copie formulario del header en pagina del footer

---
+++ admin/views/html-form-footer-view-ajax.php
@@ -0,0 +1,46 @@
+<h1>Footer</h1>
+
+<p class="comments-about-section">En esta sección usted podrá editar partes del footer global que desea mostrar
+    a lo largo de todo su multisitio.</p>
+
+<hr>
+
+<div>
+    
+    <!-- Podria agregarse algun placeholder -->
+    <div class="general-form-field">
+        <h2 class="filds-titles"> Texto </h2>
+        <input type="text">
+    </div>
+
+    <hr>
+
+    <div class="general-form-field">
+        <h2 class="filds-titles"> Enlace del texto </h2>
+        <input type="text">
+    </div>
+
+    <hr>
+
+    <div class="general-form-field">
+        <h2 class="filds-titles"> Imagen </h2>
+        <input type="file">
+    </div>
+
+    <hr>
+
+    <div class="general-form-field">
+        <h2 class="filds-titles"> Enlace de la imagen </h2>
+        <input type="text">
+    </div>
+
+    <hr>
+
+    <div class="general-form-field">
+        <button id="save-changes-button" type="button">Guardar cambios</button>
+    </div>
+     
+
+</div>
+
+<hr>
--- admin/views/html-form-header-view-ajax.php
+++ admin/views/html-form-header-view-ajax.php
@@ -1,31 +1,40 @@
 <h1>Header</h1>
 
+<p class="comments-about-section">En esta sección usted podrá editar partes del header global que desea mostrar
+    a lo largo de todo su multisitio.</p>
+
 <hr>
 
 <div>
     
+    <!-- Podria agregarse algun placeholder -->
     <div class="general-form-field">
         <h2 class="filds-titles"> Texto </h2>
         <input type="text">
     </div>
 
+    <hr>
 
     <div class="general-form-field">
         <h2 class="filds-titles"> Enlace del texto </h2>
         <input type="text">
     </div>
 
+    <hr>
 
     <div class="general-form-field">
         <h2 class="filds-titles"> Imagen </h2>
         <input type="file">
     </div>
 
+    <hr>
+
     <div class="general-form-field">
         <h2 class="filds-titles"> Enlace de la imagen </h2>
         <input type="text">
     </div>
 
+    <hr>
 
     <div class="general-form-field">
         <button id="save-changes-button" type="button">Guardar cambios</button>
