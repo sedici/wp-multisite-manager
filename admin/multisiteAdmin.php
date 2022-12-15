1f09002300bde6fc5f18ad1e3d1f65886a3e5ca3
Pablo Gabriel Terrone <pabloterrone05@gmail.com>
2022-12-15 10:06:23 -0300

    Merge de commits

--- admin/multisiteAdmin.php
+++ admin/multisiteAdmin.php
@@ -3,11 +3,18 @@ namespace Wp_multisite_manager\Admin;
 class multisiteAdmin{
 
     public function __construct() {
+        add_action('template_redirect', 'check_for_event_submissions');
     }
 
     
     public function add_admin_menu(){
         
+    }
+
+    public function check_for_event_submissions(){
+        if (isset($_POST['event'])){
+            
+        }
     }
 
     add_action( 'admin_post_add_foobar', 'prefix_admin_add_foobar' );
