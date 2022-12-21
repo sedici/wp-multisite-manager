83fb3c5a11cb2419e7aa95240e5370419368dc6a
Pablo Gabriel Terrone <pabloterrone05@gmail.com>
2022-12-21 08:54:32 -0300

    Mejoro asignación de responsabilidad en multisiteAdmin

--- admin/multisiteAdmin.php
+++ admin/multisiteAdmin.php
@@ -1,9 +1,22 @@
 <?php
 namespace Wp_multisite_manager\Admin;
+
+use Wp_multisite_manager as MM;
+
+
 class multisiteAdmin{
 
     public function __construct() {
-        add_action('template_redirect', 'check_for_event_submissions');
+
+        $this->plugin_name = MM\PLUGIN_NAME;
+		$this->version = MM\PLUGIN_VERSION;
+		$this->plugin_basename = MM\PLUGIN_BASENAME;
+		$this->plugin_text_domain = MM\PLUGIN_TEXT_DOMAIN;
+      
+        #  Esta función sirve para saber si se realizo algún POST de un formulario
+        #  add_action('template_redirect', 'check_for_event_submissions');
+      
+        add_action('network_admin_menu',array($this,'add_Multisite_Menu_Pages'),30); 
         add_action( 'admin_init', array($this,'header_settings'), 30 );
 
     }
@@ -15,11 +28,12 @@ class multisiteAdmin{
 
 
     // Function to check if a Form was submitted
-    public function check_for_event_submissions(){
+  /*  public function check_for_event_submissions(){
         if (isset($_POST['event'])){
             
         }
     }
+    */
 
     // Register all the header settings
     function header_settings() {
@@ -27,6 +41,65 @@ class multisiteAdmin{
         register_setting( 'header_settings', 'title_link' );
 
     }
+
+    # Register all the MULTISITE Menu pages --------------------------------------------------------------
+
+	public function add_Multisite_Menu_Pages(){
+		add_menu_page(__('Administrar Footer y Header', $this->plugin_text_domain),
+		__('Configurar multisitio', $this->plugin_text_domain), 
+			'manage_options',
+			$this->plugin_name,  
+			array($this, 'wp_multisite_manager_blocks')
+        );
+		$this->add_block_subpages();
+	}
+	
+	
+	public function wp_multisite_manager_blocks()
+    {
+        $url=plugins_url();
+        echo "<h1> Administrar la red de multisitio </h1>
+        <p>Este plugin de Wordpress permite administrar configuraciones gloables para todos los sitios
+		dentro de una red de Multisitio. </p>
+        ";
+		#  <a href=$url/wp-dspace/UtilizaciondelPLuginWP-Dspace.docx>Descargar Manual</a>
+        
+    }
+
+	private function add_block_subpages(){
+
+		## Agregar subpágina HEADER
+		$ajax_form_page_hook = add_submenu_page(
+            $this->plugin_name, //parent slug
+            __('Header', $this->plugin_text_domain), //page title
+            __('Header', $this->plugin_text_domain), //menu title
+            'manage_options', //capability
+            'config-header', //menu_slug
+            array($this, 'ajax_form_header_page_content')// pagina que va a manejar la sección
+        );
+
+		## Agregar subpágina FOOTER
+		$ajax_form_page_hook = add_submenu_page(
+            $this->plugin_name, //parent slug
+            __('Footer', $this->plugin_text_domain), //page title
+            __('Footer', $this->plugin_text_domain), //menu title
+            'manage_options', //capability
+            'config-footer', //menu_slug
+            array($this, 'ajax_form_footer_page_content') // pagina que va a manejar la sección
+        );
+	}
+
+	public function ajax_form_header_page_content()
+    {
+        include_once dirname(__DIR__) . '/admin/views/html-form-header-view-ajax.php';
+    }
+
+	
+	public function ajax_form_footer_page_content()
+    {
+        include_once dirname(__DIR__) . '/admin/views/html-form-footer-view-ajax.php';
+    }
+
 
 }
 
--- admin/views/html-form-header-view-ajax.php
+++ admin/views/html-form-header-view-ajax.php
@@ -6,24 +6,27 @@
 <hr>
    
 <!-- Falta agregar atributos name a los elementos del formulario -->
-<form>
+<form method="POST" >
+    <?php settings_fields( 'header_settings' ); ?>
+    <?php do_settings_sections( 'header_settings' ); ?>
+    
 <div>
     
     <!-- Podria agregarse algun placeholder -->
     <div class="general-form-field">
-        <h2 class="filds-titles"> Texto <?php plugin_basename($file); ?> </h2>
-        <input type="text">
+        <h2 class="filds-titles"> Texto </h2>
+        <input type="text" name="title-text" value="<?php echo esc_attr( get_option('title_text') ); ?>" >
     </div>
 
     <hr>
 
     <div class="general-form-field">
         <h2 class="filds-titles"> Enlace del texto </h2>
-        <input type="text">
+        <input name="title-link" type="text" value="<?php echo esc_attr( get_option('title_link') ); ?>" >
     </div>
 
     <hr>
-
+<!--
     <div class="general-form-field">
         <h2 class="filds-titles"> Imagen </h2>
         <input type="file">
@@ -35,12 +38,12 @@
         <h2 class="filds-titles"> Enlace de la imagen </h2>
         <input type="text">
     </div>
-
+-->
     <hr>
-
-    <div class="general-form-field">
+    <?php submit_button("Guardar cambios") ?>
+    <!-- <div class="general-form-field">
         <button id="save-changes-button" type="button">Guardar cambios</button>
-    </div>
+    </div> --> 
      
 
 </div>
