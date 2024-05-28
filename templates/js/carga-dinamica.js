bfe37a63de4596944421eaf6958279aaf0566e94
Pablo Gabriel Terrone <pabloterrone05@gmail.com>
2024-05-28 11:23:46 -0300

    Corrijo error con boton de portfolio. Ticket #10711

--- templates/js/carga-dinamica.js
+++ templates/js/carga-dinamica.js
@@ -2,47 +2,37 @@
 
 /* Se recibe la ultima posicion usada del arreglo array_sitios
  y el maximo del arreglo (sitios cargados) */
-(function($){
+ (function($){
     $(document).on('click','.show-more',function(e){
         e.preventDefault();
 
-		var count = $("#portfolio-count").val();
-
+        var count = $("#portfolio-count").val();
         var pagesCount = Math.ceil(cd_vars.tam_max / 3);
-
-		var box_color = cd_vars.box_color;
-
+        var box_color = cd_vars.box_color;
         var the_url = cd_vars.url;
-	
-		var auxCount = parseInt(count);
-		auxCount++;
+        var auxCount = parseInt(count);
+        auxCount++;
+
         jQuery.ajax({
-			url : the_url,
+            url: the_url,
             type: 'post',
-			data : {
-				actual_count: auxCount,
-				box_color:box_color,
-				action: 'load_more'
-			},
-			success: function(response){
-				$("#portfolio-count").val(auxCount);
-				$(".sites-portfolio").append(response)
-				if(auxCount++ >= pagesCount){
-					$('.show-more').hide();
-				}
-			},
-
-			error : function(e){
-				console.log('An error occurred sending the data');
-			}
-
-		})
-
-		})	
-
-
-        
-
-    })
-
-(jQuery);
+            data: {
+                actual_count: auxCount,
+                box_color: box_color,
+                action: 'load_more'
+            },
+            success: function(response){
+                $("#portfolio-count").val(auxCount);
+                $(".sites-portfolio").append(response);
+                // Muevo el botón "show-more" después de agregar las nuevas cajas
+                $(".show-more").appendTo(".sites-portfolio");
+                if(auxCount >= pagesCount){
+                    $('.show-more').hide();
+                }
+            },
+            error: function(e){
+                console.log('An error occurred sending the data');
+            }
+        });
+    });
+})(jQuery);
